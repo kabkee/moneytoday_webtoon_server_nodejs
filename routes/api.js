f0cdba11d7634476e693be51c75250169c000b42
Kabkee Moon <kabkee@live.com>
2020-06-13 02:27:27 +0900

    api 완성

--- routes/api.js
+++ routes/api.js
@@ -1,45 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const dataFolder = 'data';
+const dataFolder = 'public/data';
 let toons = [];
 
 routeInit();
 
-function routeInit(){
-  fs.readFile('api_data_meta.json', (err, data) => {
-    if (err) throw err;
-    toons = JSON.parse(data);
-  });
+function routeInit() {
+    fs.readFile('api_data_meta.json', (err, data) => {
+        if (err) throw err;
+        toons = JSON.parse(data);
+    });
 }
 
 /* GET webtoon listing. */
 router.get('/list', function(req, res, next) {
-  res.json(toons);
-});
-router.get('/toon/:toonIdx', function(req, res, next) {
-  let toonIdx = req.params.toonIdx;
-  res.json(toons[toonIdx]);
-  return;
-  
-  fs.readFile('data_meta.json', (err, data) => {
-    if (err) throw err;
-    let toons = JSON.parse(data);
-    console.log(toons);
     res.json(toons);
-  });
 });
-
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-  
-  fs.readdir(dataFolder, function(error, filelist){
-    if(error){
-      console.error(error)
-    }
-    console.log(filelist);
-  })
-
+router.get('/list/:toonIdx', function(req, res, next) {
+    let toonIdx = req.params.toonIdx;
+    res.json(toons[toonIdx]);
+});
+router.get('/chapter/:toonIdx', function(req, res, next) {
+    let toonIdx = req.params.toonIdx;
+    fs.readFile(`${dataFolder}/${toons[toonIdx].title}/api_chapter_meta.json`, (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
 });
 
 module.exports = router;
