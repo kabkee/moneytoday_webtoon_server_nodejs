235feb0d09169a6083ea6ea8ed5b31c9f500f3f4
Kabkee Moon <kabkee@live.com>
2020-06-17 17:56:58 +0900

    vue history mode 서포트 미드웨어추가

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var history = require('connect-history-api-fallback');
+
 
 // var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
@@ -17,6 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(history());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
