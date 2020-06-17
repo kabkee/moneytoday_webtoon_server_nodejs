9fb1030ea4e0f51c1c03915eade947e8cc945ab9
Kabkee Moon <kabkee@live.com>
2020-06-17 16:45:39 +0900

    api 개발 완료

--- routes/api.js
+++ routes/api.js
@@ -17,16 +17,27 @@ function routeInit() {
 router.get('/list', function(req, res, next) {
     res.json(toons);
 });
-router.get('/list/:toonIdx', function(req, res, next) {
-    let toonIdx = req.params.toonIdx;
-    res.json(toons[toonIdx]);
-});
-router.get('/chapter/:toonIdx', function(req, res, next) {
-    let toonIdx = req.params.toonIdx;
-    fs.readFile(`${dataFolder}/${toons[toonIdx].title}/api_chapter_meta.json`, (err, data) => {
-        if (err) throw err;
-        res.json(JSON.parse(data));
-    });
+// router.get('/list/:toonIdx', function(req, res, next) {
+//     let toonIdx = req.params.toonIdx;
+//     res.json(toons[toonIdx]);
+// });
+router.get('/chapter/:toonId', function(req, res, next) {
+    let toonId = req.params.toonId;
+    let toonData = null
+    for (var i = 0; i < toons.length; i++) {
+        if (toons[i].id == toonId) {
+            toonData = toons[i];
+            break;
+        }
+    }
+    if (toonData) {
+        fs.readFile(`${dataFolder}/${toonData.title}/api_chapter_meta.json`, (err, data) => {
+            if (err) throw err;
+            res.json(JSON.parse(data));
+        });
+    } else {
+        res.json({});
+    }
 });
 
 module.exports = router;
